c0d220416ea1354ab61c979efdeee3361efb68ca
Lucas Tavares <32081433+lucast98@users.noreply.github.com>
2020-09-07 20:18:18 -0300

    Add files via upload

---
+++ Project 1/ex2-p1.R
@@ -0,0 +1,21 @@
+library(XLConnect)
+library(lubridate)
+library(zoo)
+library(ggplot2)
+
+#importar os dados
+#setwd("/home/lucas/")
+wb <- loadWorkbook("ipeadata[07-09-2020-02-32].xls")
+Matriz <- readWorksheet(wb, sheet = "Séries", startRow = 0, startCol = 0)
+
+#alterar vetor de datas para o formato certo
+names(Matriz) <- c("Data", "IBOVESPA")
+Matriz$Data <- parse_date_time(Matriz$Data, "%d/%m/%Y")
+
+#transformando os dados em séries temporais
+tsIBOVESPA <- zoo(Matriz$IBOVESPA,Matriz$Data)
+
+#plotar grafico com ggplot2
+ss <- subset(Matriz, Data > as.Date("2005-1-1"))
+p <- ggplot(ss, aes(x=Data, y=IBOVESPA)) + geom_line(color="blue") + theme_linedraw()
+p + ggtitle("Índice de ações da IBOVESPA em função da data") + xlab("Data") + ylab("Índice de ações - Ibovespa")
